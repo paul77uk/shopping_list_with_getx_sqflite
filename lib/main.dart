ac08cb6821a5cd84bc60a4ca5bdec03b6dc63f13
paul77uk <paul77uk@gmail.com>
2020-08-11 17:07:02 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,8 @@ import 'ui/shopping_list_screen.dart';
 void main() => runApp(
       GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        home: MyApp(),
+        home: ShList(),
       ),
     );
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Shoppping List',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: ShList());
-  }
-}
+
